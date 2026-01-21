6965b7b1bf7e09f9994d0b2ad173bbb0bae05b37
willsamartin-project-manager <willsamartin@gmail.com>
2026-01-21 10:04:07 -0300

    useNavigate is not defined

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useSocket } from '../context/SocketContext';
 import { Trash2, Bell, CheckCircle, QrCode } from 'lucide-react';
 import QRCode from "react-qr-code";
